feb324d8e88ecf6528cc18612c592862c1b4b2ec
copety <copety@oregonstate.edu>
2017-08-12 16:33:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,4 @@ $(document).ready(function(){
         $.ajax({
         type: "POST",
         url: "~/example.py"        
-        }).done(function(e) {
-            alert("Post to example.py successful");
-        });
-}}));
+        })}};
