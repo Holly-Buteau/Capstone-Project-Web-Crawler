6e75abd39f4c9ae9943ee7a88bb06e9156ac37bc
copety <copety@oregonstate.edu>
2017-08-12 17:16:55 -0400

    Add files via upload

---
+++ formtest.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>PHP Form Test</title>
+        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Allerta" />
+    </head>
+    <body>
+        <?php
+
+        $search_type = $_POST['search_type'];        
+        
+
+        </div>
+    </body>
+</html>
