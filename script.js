ffeb63209bea910e81a4469d331667ce8a6d00ee
copety <copety@oregonstate.edu>
2017-08-13 20:38:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
   $("#submitButton").click(function(e){
       e.preventDefault();
+      var searchString = "";
+      var selected = $("input[type='radio'][name='searchType']:checked");
+      searchString = selected.val();
         $.ajax({type: "POST",
             url: "/formtest.php",
-            data: { search_type: "testValue" },
+            data: { search_type: searchString },
             success:function(result){
               alert('ok');
             },
