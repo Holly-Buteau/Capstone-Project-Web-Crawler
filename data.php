525384d3de1e8819cb57c12086657c115e9bf8b1
copety <copety@oregonstate.edu>
2017-08-13 21:44:19 -0400

    Add files via upload

---
+++ data.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>PHP Database Connect</title>
+        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Allerta" />
+    </head>
+    <body>
+        <?php
+         header('Content-type: application/json; charset=utf-8');
+       $db_connection = pg_connect("host=firstdbinstance.cxvcjdies8vv.us-east-2.rds.amazonaws.com dbname=beginning_database user=copety  password=eridanus");
+       $result = pg_query($db_connection, "SELECT * FROM webholder");
+       $data = array();
+    while ($row = pg_fetch_array($result))
+    { 
+      $data[] = $row;
+    } 
+    echo json_encode($data);
+    pg_close($db_connection);
+        
+        ?>
+    </body>
+</html>
