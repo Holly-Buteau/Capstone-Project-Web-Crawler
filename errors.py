34757e65f94c06468dea87ea877d542b5ebc9260
copety <copety@oregonstate.edu>
2017-08-14 16:03:01 -0400

    Update errors.py

--- errors.py
+++ errors.py
@@ -1,28 +1,13 @@
-#!/usr/bin/env python
-import sys
-import json
-import cgi
+# General
+class ArgumentError(Exception): pass
 
-fs = cgi.FieldStorage()
+# Persistence
+class PersistenceError(Exception):
+    def __init__(self, code=1, msg='Unknown'):
+        self.code = code
+        self.msg = msg
 
-sys.stdout.write("Content-Type: application/json")
-
-sys.stdout.write("\n")
-sys.stdout.write("\n")
-
-
-result = {}
-result['success'] = True
-result['message'] = "The command Completed Successfully"
-result['keys'] = ",".join(fs.keys())
-
-d = {}
-for k in fs.keys():
-    d[k] = fs.getvalue(k)
-
-result['data'] = d
-
-sys.stdout.write(json.dumps(result,indent=1))
-sys.stdout.write("\n")
-
-sys.stdout.close()
+    def __str__(self):
+        return 'ERR: CODE: %s ; MSG: %s' %(self.code, self.msg)
+class PersistenceExecuteError(PersistenceError): pass
+class PersistenceConnectionError(PersistenceError): pass
