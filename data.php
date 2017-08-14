a486947cae81deeaa7b42e0c54d4f31b2d03185f
copety <copety@oregonstate.edu>
2017-08-14 16:14:38 -0400

    Update data.php

--- data.php
+++ data.php
@@ -4,5 +4,9 @@
         <link rel="stylesheet" href="styles.css">
         <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Allerta" />
     </head>
-    
+     <body>
+        <?php
+         header('Content-type: application/json; charset=utf-8');
+         $db_connection = pg_connect("host = firstdbinstance.cxvcjdies8vv.us-east-2.rds.amazonaws.com dbname = beginning_database user = copety  password = eridanus port = 5432");
+     ?>
 </html>
